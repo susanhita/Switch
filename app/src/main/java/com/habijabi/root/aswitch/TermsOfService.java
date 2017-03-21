e2ed9640afe2129f6785414470464e272bd739d5
root <susanhita@gmail.com>
2017-03-21 14:03:22 +0530

    terms of services

--- app/src/main/java/com/habijabi/root/aswitch/TermsOfService.java
+++ app/src/main/java/com/habijabi/root/aswitch/TermsOfService.java
@@ -8,6 +8,8 @@ import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,17 +19,24 @@ public class TermsOfService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_of_service);
+        overridePendingTransition(R.anim.transition,R.anim.transition_reverse);
         TextView textView =(TextView)findViewById(R.id.textView4);
         textView.setClickable(true);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
-        String text = "To use Dhitra, you must agree to the <a href='http://www.calm.com'> Terms of Services </a> and read the <a href='http://www.apartmenttherapy.com'> Privacy Policy. </a>";
+        String text = "To use Dhitra, you must agree to the <a href='switch://www.seventeen.com'> Terms of Services</a>";
         textView.setText(Html.fromHtml(text));
+
+        TextView privacy =(TextView)findViewById(R.id.privacy);
+        privacy.setClickable(true);
+        privacy.setMovementMethod(LinkMovementMethod.getInstance());
+        String priv = "and read the <a href='switch://www.apartmenttherapy.com'> Privacy Policy </a>";
+        privacy.setText(Html.fromHtml(priv));
 
     }
 
     public void info(View view){
         Intent browserIntent=new Intent(this,WebViewActivity.class);
-       // Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+        browserIntent.setData(Uri.parse("switch://www.abeautifulmess.com"));
         startActivity(browserIntent);
     }
 
--- app/src/main/java/com/habijabi/root/aswitch/WebViewActivity.java
+++ app/src/main/java/com/habijabi/root/aswitch/WebViewActivity.java
@@ -1,7 +1,10 @@
 package com.habijabi.root.aswitch;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 public class WebViewActivity extends AppCompatActivity {
@@ -12,11 +15,33 @@ public class WebViewActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-
+        overridePendingTransition(R.anim.transition,R.anim.transition_reverse);
+        Log.v("yayyyyyyyyyyyyyyyy","reached yere");
         webView = (WebView) findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://www.abeautifulmess.com");
+        //webView.loadUrl("http://www.abeautifulmess.com");
+        if( savedInstanceState == null ) {
+            String url =
+                    getIntent().getDataString().replace("switch://", "http://");
+            webView.loadUrl(url);
 
+        }
+    }
+
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.coming_in, R.anim.coming_out);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return(super.onOptionsItemSelected(item));
     }
 
 }
